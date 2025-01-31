578fd0b1ea70c91b1f2feb7e23bcbb9a568f3612
mathiaspedro9 <mathiaspedro9@gmail.com>
2025-01-31 12:58:32 +0000

    fix

--- controle-atendimento-petshop-web/src/app/pages/listagens/visualizar-pets/visualizar-pets.component.ts
+++ controle-atendimento-petshop-web/src/app/pages/listagens/visualizar-pets/visualizar-pets.component.ts
@@ -2,7 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { CadastroPetComponent } from "../../../shared/components/forms/cadastros/cadastro-pet/cadastro-pet.component";
 import { ModalComponent } from "../../../shared/components/page/modal/modal.component";
-import { Pet, Raca } from '../../../shared/interfaces/petshop.entities';
+import { Cliente, Pet, Raca } from '../../../shared/interfaces/petshop.entities';
 import { PetshopService } from '../../../shared/services/petshop.service';
 
 @Component({
@@ -41,28 +41,38 @@ export class VisualizarPetsComponent implements OnInit {
   }
 
   petAdicionado(pet: Pet) {
-    if (pet.id && pet.id > 0) pet.cliente = this.pets.find(p => p.id === pet.id)!.cliente;
+    this.service.listarClientes({ qtd: 10, page: 1 }).subscribe({
+      next: (clientes: Cliente[]) => {
+        if (pet.id && pet.id > 0) {
+          pet.cliente = clientes.find(p => p.id === pet.id) as Cliente
+          pet.cliente.dataCadastro = (pet.cliente.dataCadastro as Date).toISOString().split('T')[0];
+          pet.cliente.pets = [];
+        }
 
-    this.service.cadastrarPet(pet).subscribe({
-      next: (data: Pet) => {
-        this.isLoading = true;
+        this.service.cadastrarPet(pet).subscribe({
+          next: (data: Pet) => {
+            this.isLoading = true;
 
-        this.service.listarPets({ qtd: 10, page: 1 }).subscribe({
-          next: (pets: Pet[]) => this.pets = pets,
+            this.service.listarPets({ qtd: 10, page: 1 }).subscribe({
+              next: (pets: Pet[]) => this.pets = pets,
+              error: (err: HttpErrorResponse) => console.error(err),
+              complete: () => this.isLoading = false,
+            });
+
+            this.service.listarRacas({ qtd: 10, page: 1 }).subscribe({
+              next: (racas: Raca[]) => this.racas = racas,
+              error: (err: HttpErrorResponse) => console.error(err),
+            });
+
+            this.openModalAtualizarPet = false;
+            this.petSelecionadoEdicao = undefined;
+          },
           error: (err: HttpErrorResponse) => console.error(err),
-          complete: () => this.isLoading = false,
         });
-
-        this.service.listarRacas({ qtd: 10, page: 1 }).subscribe({
-          next: (racas: Raca[]) => this.racas = racas,
-          error: (err: HttpErrorResponse) => console.error(err),
-        });
-
-        this.openModalAtualizarPet = false;
-        this.petSelecionadoEdicao = undefined;
       },
       error: (err: HttpErrorResponse) => console.error(err),
-    }); 
+      complete: () => this.isLoading = false,
+    });
   }
 
   editarPet(pet: Pet) {
